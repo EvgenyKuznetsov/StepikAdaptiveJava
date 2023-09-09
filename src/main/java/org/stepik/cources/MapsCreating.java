b12ff616cec548584b8228d7c963ebcfc4ee7295
Evgeny Kuznetsov <evg_kuznetsov@vk.com>
2023-09-09 17:35:43 +0500

    3.114 Maps. Creating.

---
+++ src/main/java/org/stepik/cources/MapsCreating.java
@@ -0,0 +1,20 @@
+package org.stepik.cources;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/*
+* 3.114 Maps. Creating.
+ */
+
+public class MapsCreating {
+    public static void main(String[] args){
+        Map<String, Integer> map = new TreeMap<>();
+        map.put("Omega", 24);
+        map.put("Alpha", 1);
+        map.put("Gamma", 3);
+        
+        System.out.println(map);
+    }
+    
+}
